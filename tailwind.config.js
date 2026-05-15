6770029fc8360500a9b95f2c631c3d5841243bc0
Laura Camila Diaz Delgado <lauracamiladiazdelgado@gmail.com>
2026-05-15 15:17:50 -0500

    animaciones

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,12 +20,12 @@ export default {
       },
 
       animation: {
-        fade: 'fadeInUp 1s both',
+        fade: 'fadeInUp 350ms both',
       },
 
       keyframes: {
         fadeInUp: {
-          '0%': { opacity: 0, transform: 'translateY(2rem)' },
+          '0%': { opacity: 0, transform: 'translateY(0.75rem)' },
           '100%': { opacity: 1, transform: 'translateY(0)' },
         },
       },
