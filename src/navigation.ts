b5a104713e04f458a126e5e9987687fbeb6d70e7
Laura Camila Diaz Delgado <lauracamiladiazdelgado@gmail.com>
2026-05-15 15:59:33 -0500

    footer

--- src/navigation.ts
+++ src/navigation.ts
@@ -45,9 +45,7 @@ export const footerData = {
   links: [],
   secondaryLinks: [],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/hdspgroup/' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/hdspgroup' },
   ],
